66c7d4b5fd362edadadc31981760b813a99fe8a6
Reyn12 <muhammadrenaldi687@gmail.com>
2025-01-21 11:19:42 +0700

    sidebar oke

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Manajemen Barang')</title>
-    
-    <!-- Tailwind CSS via CDN -->
-    <script src="https://cdn.tailwindcss.com"></script>
-    
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
     <!-- Font Lato dari Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
-    
+
     <!-- Custom Style -->
     <style>
         body {
@@ -20,7 +22,7 @@
 
     @stack('styles')
 </head>
-<body class="bg-gray-100">
+<body class="bg-gray-50">
     @yield('content')
     @stack('scripts')
 </body>
